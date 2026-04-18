6480eabbba628767a4e6a722a64397cd4e5a2289
n <norah.zhao24@gmail.com>
2026-04-17 22:19:26 -0700

    Delete acne_classifier/__pycache__/backend directory

--- acne_classifier/__pycache__/backend/inspect_classes.py
+++
@@ -1,9 +0,0 @@
-import torch
-from model_def import AcneModel
-
-model = AcneModel()
-state = torch.load("acne_model.pt", map_location="cpu")
-model.load_state_dict(state)
-
-print(model.backbone.classifier)
-
--- acne_classifier/__pycache__/backend/predict_test.py
+++
@@ -1,9 +0,0 @@
-import torch
-from model_def import AcneModel
-
-model = AcneModel()  # no arguments
-state = torch.load("acne_model.pt", map_location="cpu")
-model.load_state_dict(state)
-model.eval()
-
-print("Model loaded successfully!")
